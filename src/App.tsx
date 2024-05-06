af417a789b9d8baaf73d3b3b2c9cb0f2ea28391f
bkm-code <bridgit.k.mutinda@gmail.com>
2024-05-06 18:34:51 +0300

    Create website pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
+import Products from "./pages/Products/Products";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import './App.css';
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Home</div>,
-  },
-  {
-    path: "/products/:id",
-    element: <div>Category</div>,
-  },
-  {
-    path: "/product/:id",
-    element: <div>Product</div>,
-  },
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+    ]
+  }
 ]);
 
 const App = () => {
