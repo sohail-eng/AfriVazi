982cf993c8c424b98002eb0272ff8e2d4952635c
bkm-code <bridgit.k.mutinda@gmail.com>
2024-05-14 23:51:09 +0300

    Add interactivity to product page sidebar

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Products.scss";
 import all08 from "./../../assets/image/all08.jpg";
 import List from "../../components/List/List";
 
 const Products = () => {
+  const categoryId = parseInt(useParams().id);
+
+  const [maxPrice, setMaxPrice] = useState(1000);
+
+  const [sort, setSort] = useState(null);
+
   return (
     <div className="products">
       <div className="left">
@@ -30,25 +36,42 @@ const Products = () => {
           <h2>Filter by price</h2>
           <div className="inputItem">
             <span>1</span>
-            <input type="range" min={0} max={1000} />
-            <span>1000</span>
+            <input
+              type="range"
+              min={0}
+              max={1000}
+              onChange={(e) => sstMaxPrice(e.target.value)}
+            />
+            <span>{maxPrice}</span>
           </div>
         </div>
         <div className="filterItem">
           <h2>Sort by</h2>
           <div className="inputItem">
-            <input type="radio" id="asc" value="asc" name="price" />
+            <input
+              type="radio"
+              id="asc"
+              value="asc"
+              name="price"
+              onChange={(e) => setSort("asc")}
+            />
             <label htmlFor="asc">Price (Lowest first)</label>
           </div>
           <div className="inputItem">
-            <input type="radio" id="desc" value="desc" name="price" />
+            <input
+              type="radio"
+              id="desc"
+              value="desc"
+              name="price"
+              onChange={(e) => setSort("desc")}
+            />
             <label htmlFor="desc">Price (Highest first)</label>
           </div>
         </div>
       </div>
       <div className="right">
         <img className="categoryImage" src={all08} alt="" />
-        <List />
+        <List categoryId={categoryId} maxPrice={maxPrice} sort={sort} />
       </div>
     </div>
   );
