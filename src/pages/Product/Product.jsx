b545dff7053eee4619580048e650ae8d19952eba
bkm-code <bridgit.k.mutinda@gmail.com>
2024-05-15 14:48:28 +0300

    Single products page images section UI

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
 import "./Product.scss";
+import accessories06 from "./../../assets/image/accessories06.jpg";
+import accessories07 from "./../../assets/image/accessories07.jpg";
 
 const Product = () => {
-  return (
-    <div className='product'>Product</div>
-  )
-}
 
-export default Product
+  const [selectedImg,setSelectedImg] = useState(0)
+
+  const Images = [accessories06, accessories07];
+
+  return (
+    <div className="product">
+      <div className="left">
+        <div className="images">
+          <img src={Images[0]} alt="" onClick={e=>setSelectedImg(0)} />
+          <img src={Images[1]} alt="" onClick={e=>setSelectedImg(1)} />
+        </div>
+        <div className="mainImg">
+          <img src={Images[setSelectedImg]} alt="" />
+        </div>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
+};
+
+export default Product;
