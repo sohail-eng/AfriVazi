a2d36a859fbedb9b76e0671b51d52099a9ee8250
bkm-code <bridgit.k.mutinda@gmail.com>
2024-05-27 12:24:28 +0300

    Added useEffect hook to automatically transition to the next slide every 3 seconds.

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,13 +1,12 @@
-import React, { useState } from "react";
-import accessories02 from "./../../assets/image/accessories02.jpg";
+import React, { useEffect, useState } from "react";
 import all01 from "./../../assets/image/all01.jpg";
-import all03 from "./../../assets/image/all03.jpg";
-import kids06 from "./../../assets/image/kids06.jpg";
+import accessories13 from "./../../assets/image/accessories13.jpg";
+import all05 from "./../../assets/image/all05.jpg";
+import all10 from "./../../assets/image/all10.jpg";
 import king06 from "./../../assets/image/king06.jpg";
-import kingnqueen01 from "./../../assets/image/kingnqueen01.jpg";
-import kingnqueen05 from "./../../assets/image/kingnqueen05.jpg";
-import queen01 from "./../../assets/image/queen01.jpg";
-import queen07 from "./../../assets/image/queen07.jpg";
+import familia03 from "./../../assets/image/familia03.jpg";
+import king07 from "./../../assets/image/king07.jpg";
+import accessories17 from "./../../assets/image/accessories17.jpg";
 import queen13 from "./../../assets/image/queen13.jpg";
 import WestOutlinedIcon from "@mui/icons-material/WestOutlined";
 import EastOutlinedIcon from "@mui/icons-material/EastOutlined";
@@ -15,36 +14,58 @@ import "./Slider.scss";
 
 const Slider = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [transitioning, setTransitioning] = useState(false);
 
   const Data = [
-    queen01,
-    accessories02,
     all01,
-    kingnqueen01,
-    all03,
-    queen07,
-    kids06,
+    accessories13,
+    all05,
+    all10,
     king06,
-    kingnqueen05,
+    familia03,
+    king07,
+    accessories17,
     queen13,
   ];
 
   const prevSlide = () => {
-    setCurrentSlide((prev) => (prev === 0 ? Data.length - 1 : prev - 1));
+    if (currentSlide === 0) {
+      setCurrentSlide(Data.length - 1);
+    } else {
+      setCurrentSlide(currentSlide - 1);
+    }
   };
 
   const nextSlide = () => {
-    setCurrentSlide((prev) => (prev === Data.length - 1 ? 0 : prev + 1));
+    if (currentSlide === Data.length - 1) {
+      setCurrentSlide(0);
+    } else {
+      setCurrentSlide(currentSlide + 1);
+    }
   };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      nextSlide();
+    }, 3000); // Change slide every 3 seconds
+
+    return () => clearInterval(interval);
+  }, [currentSlide]);
 
   return (
     <div className="slider">
       <div
         className="container"
-        style={{ transform: `translateX(-${currentSlide * 100}%)` }}
+        style={{
+          transform: `translateX(-${currentSlide * 100}vw)`,
+          transition:
+            currentSlide === 0 || currentSlide === Data.length - 1
+              ? "none"
+              : "transform 1s ease",
+        }}
       >
-        {Data.map((image, index) => (
-          <img src={image} alt={`Slide ${index}`} key={index} />
+        {Data.map((img, index) => (
+          <img key={index} src={img} alt={`Slide ${index}`} />
         ))}
       </div>
       <div className="icons">
