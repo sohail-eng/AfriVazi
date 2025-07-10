bffccbfaa4a4cb80c67f4efdd9898c13a9e34ba2
bridgitkanini <bridgit.k.mutinda@gmail.com>
2025-07-11 02:47:00 +0300

    Refactor List and Products components to utilize useFetch for dynamic data fetching, enabling product filtering by subcategories and improving loading/error handling.

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -1,57 +1,19 @@
 import React from "react";
 import './List.scss';
 import Card from "../Card/Card";
-import accessories04 from "./../../assets/image/accessories04.jpg";
-import queen11 from "./../../assets/image/queen11.jpg";
-import queen09 from "./../../assets/image/queen09.jpg";
-import queen10 from "./../../assets/image/queen10.jpg";
-import king02 from "./../../assets/image/king02.jpg";
-import king04 from "./../../assets/image/king04.jpg";
-import kids05 from "./../../assets/image/kids05.jpg";
-import kids06 from "./../../assets/image/kids06.jpg";
+import useFetch from "../../hooks/useFetch";
 
-const List = () => {
-  const Data = [
-    {
-      id: 1,
-      img: queen09,
-      img2: queen10,
-      title: "Queen's Yellow Ankara Dress",
-      isNew: true,
-      oldPrice: 40,
-      price: 35,
-    },
-    {
-      id: 2,
-      img: king04,
-      img2: king02,
-      title: "King's Kaftan Black and White",
-      isNew: true,
-      oldPrice: 42,
-      price: 37,
-    },
-    {
-      id: 3,
-      img: accessories04,
-      img2: queen11,
-      title: "Queen's Headwrap",
-      isNew: true,
-      oldPrice: 24,
-      price: 17,
-    },
-    {
-      id: 4,
-      img: kids06,
-      img2: kids05,
-      title: "Toto's Dashiki Shirt and Tights",
-      isNew: true,
-      oldPrice: 38,
-      price: 33,
-    },
-  ];
+const List = ({ subCategories, maxPrice, sort, categoryId }) => {
+
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&[filters][categories][id]=${categoryId}${subCategories.map(
+      (item) => `&[filters][sub_categories][id][$eq]=${item}`
+    )}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`
+  );
+  
   return (
     <div className="list">
-      {Data?.map((item) => (
+      {error? "Something went wrong!" : loading? "loading" : data?.map((item) => (
         <Card item={item} key={item.id} />
       ))}
     </div>
--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -3,35 +3,38 @@ import { useParams } from "react-router-dom";
 import "./Products.scss";
 import all08 from "./../../assets/image/all08.jpg";
 import List from "../../components/List/List";
+import useFetch from "../../hooks/useFetch";
 
 const Products = () => {
   const categoryId = parseInt(useParams().id);
-
   const [maxPrice, setMaxPrice] = useState(1000);
-
   const [sort, setSort] = useState(null);
+  const [selectedSubcategories, setSelectedSubcategories] = useState([]);
+
+  const { data, loading, error } = useFetch(`/sub-categories?populate=*`);
+
+  const handleChange = (e) => {
+
+    const value = e.target.value;
+    const isChecked = e.target.checked;
+    setSelectedSubcategories(
+      isChecked
+        ? [...selectedSubcategories, value]
+        : selectedSubcategories.filter((item) => item !== value)
+    );
+  };
 
   return (
     <div className="products">
       <div className="left">
         <div className="filterItem">
           <h2>Product Categories</h2>
-          <div className="inputItem">
-            <input type="checkbox" id="1" value={1} />
-            <label htmlFor="1">Accessories</label>
-          </div>
-          <div className="inputItem">
-            <input type="checkbox" id="2" value={2} />
-            <label htmlFor="1">KIng</label>
-          </div>
-          <div className="inputItem">
-            <input type="checkbox" id="3" value={3} />
-            <label htmlFor="1">Queen</label>
-          </div>
-          <div className="inputItem">
-            <input type="checkbox" id="4" value={4} />
-            <label htmlFor="1">Toto</label>
-          </div>
+          {error? "Something went wrong!" : loading? "loading" : data?.map((item) => (
+            <div className="inputItem" key={item.id}>
+              <input type="checkbox" id={item.id} value={item.id} onChange={handleChange} />
+              <label htmlFor={item.id}>{item?.title}</label>
+            </div>
+          ))}
         </div>
         <div className="filterItem">
           <h2>Filter by price</h2>
@@ -54,7 +57,7 @@ const Products = () => {
               id="asc"
               value="asc"
               name="price"
-              onChange={(e) => setSort("asc")}
+              onChange={() => setSort("asc")}
             />
             <label htmlFor="asc">Price (Lowest first)</label>
           </div>
@@ -64,7 +67,7 @@ const Products = () => {
               id="desc"
               value="desc"
               name="price"
-              onChange={(e) => setSort("desc")}
+              onChange={() => setSort("desc")}
             />
             <label htmlFor="desc">Price (Highest first)</label>
           </div>
@@ -72,7 +75,7 @@ const Products = () => {
       </div>
       <div className="right">
         <img className="categoryImage" src={all08} alt="" />
-        <List categoryId={categoryId} maxPrice={maxPrice} sort={sort} />
+        <List categoryId={categoryId} maxPrice={maxPrice} sort={sort} subCategories={selectedSubcategories} />
       </div>
     </div>
   );
