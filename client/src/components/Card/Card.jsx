434efa0519548be1578b57d1a2ff37803bd4d3e0
bridgitkanini <bridgit.k.mutinda@gmail.com>
2025-07-11 00:52:53 +0300

    Update FeaturedProducts component to fetch products from API and improve Card component image handling; update Strapi updater timestamp and jsconfig settings.

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -3,18 +3,25 @@ import "./Card.scss";
 import { Link } from "react-router-dom";
 
 const Card = ({ item }) => {
+  const imgUrl = item?.img?.url
+    ? import.meta.env.VITE_UPLOAD_URL + item.img.url
+    : "";
+  const img2Url = item?.img2?.url
+    ? import.meta.env.VITE_UPLOAD_URL + item.img2.url
+    : "";
+
   return (
     <Link className="link" to={`/product/${item.id}`} >
       <div className="card">
         <div className="image">
-            {item.isNew && <span>New Season</span> }
-          <img src={item.img} alt="" className="mainImg" />
-          <img src={item.img2} alt="" className="secondImg" />
+          {item?.isNew && <span>New Season</span>}
+          {imgUrl && <img src={imgUrl} alt="" className="mainImg" />}
+          {img2Url && <img src={img2Url} alt="" className="secondImg" />}
         </div>
-        <h2>{item.title}</h2>
+        <h2>{item?.title}</h2>
         <div className="prices">
-          <h3>${item.oldPrice}</h3>
-          <h3>${item.price}</h3>
+          <h3>${item.oldPrice || (item?.price ? item.price + 20 : "")}</h3>
+          <h3>${item?.price}</h3>
         </div>
       </div>
     </Link>
--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,67 +1,21 @@
 import React, { useEffect, useState } from "react";
 import "./FeaturedProducts.scss";
-import accessories04 from "./../../assets/image/accessories04.jpg";
-import queen11 from "./../../assets/image/queen11.jpg";
-import queen09 from "./../../assets/image/queen09.jpg";
-import queen10 from "./../../assets/image/queen10.jpg";
-import king02 from "./../../assets/image/king02.jpg";
-import king04 from "./../../assets/image/king04.jpg";
-import kids05 from "./../../assets/image/kids05.jpg";
-import kids06 from "./../../assets/image/kids06.jpg";
 import Card from "../Card/Card";
 import axios from "axios";
 
 const FeaturedProducts = ({type}) => {
-  const Data = [
-    {
-      id: 1,
-      img: queen09,
-      img2: queen10,
-      title: "Queen's Yellow Ankara Dress",
-      isNew: true,
-      oldPrice: 40,
-      price: 35,
-    },
-    {
-      id: 2,
-      img: king04,
-      img2: king02,
-      title: "King's Kaftan Black and White",
-      isNew: true,
-      oldPrice: 42,
-      price: 37,
-    },
-    {
-      id: 3,
-      img: accessories04,
-      img2: queen11,
-      title: "Queen's Headwrap",
-      isNew: true,
-      oldPrice: 24,
-      price: 17,
-    },
-    {
-      id: 4,
-      img: kids06,
-      img2: kids05,
-      title: "Toto's Dashiki Shirt and Tights",
-      isNew: true,
-      oldPrice: 38,
-      price: 33,
-    },
-  ];
-
-  const [products, setProducts] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await axios.get(import.meta.env.VITE_API_URL + "/products", {
+        const res = await axios.get(import.meta.env.VITE_API_URL + "/products?populate=*", {
           headers: {
             Authorization: "bearer " + import.meta.env.VITE_API_TOKEN,
           }
         });
-        console.log(data);
+
+        setData(res.data.data);
       } catch (err) {
         console.log(err);
       }
@@ -84,9 +38,13 @@ const FeaturedProducts = ({type}) => {
         </p>
       </div>
       <div className="bottom">
-        {Data.map((item) => (
-          <Card item={item} key={item.id} />
-        ))}
+        {data.length === 0 ? (
+          <p>No products found.</p>
+        ) : (
+          data.map((item) => (
+            <Card item={item} key={item.id} />
+          ))
+        )}
       </div>
     </div>
   );
