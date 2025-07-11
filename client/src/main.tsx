0bc6077ef4d493c56cdc1732b298506bc485a11e
bridgitkanini <bridgit.k.mutinda@gmail.com>
2025-07-11 06:23:30 +0300

    Integrate redux-persist to persist cart state across sessions, updating store configuration and wrapping App component with PersistGate for improved state management.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
+import { store, persistor } from "./redux/store.js";
+
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={"loading"} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
